ade313f7ef304e3065b437f7fbda2f16df527654
NaderNasr <nadernasr7@gmail.com>
2022-04-09 23:03:50 -0400

    render keyboard letter to buttons

---
+++ App.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-undef */
+const tileDisplay = document.querySelector('.tile_container');
+const keyboard = document.querySelector('.input_container');
+
+const keys = [
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+  '«',
+  'ENTER'
+];
+
+handleClick = () => {
+  console.log(`clicked!`);
+};
+
+keys.forEach((key) => {
+  const buttonElement = document.createElement('button');
+  buttonElement.textContent = key;
+  buttonElement.setAttribute('id', key);
+  buttonElement.addEventListener('click', handleClick);
+  keyboard.append(buttonElement);
+});
